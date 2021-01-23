a6e254fe909d4c4f46d6c39d9fd773867f5ce514
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2021-01-23 23:21:27 +0900

    ファイル構成 設計

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 状態Authと状態BoardsをVuexのstateで一元管理できるように定義する
+const state = {
+  auth: { // 状態Auth
+    token: null, // tokenはnullで初期化
+    userId: null // userIdはnullで初期化
+  },
+  board: { // 状態Board
+    lists: [] // 状態taskListは空で初期化
+  }
+}
+
 export default new Vuex.Store({
+  state, // 定義したstateをstateオプションに指定
   getters,
   actions,
   mutations,
