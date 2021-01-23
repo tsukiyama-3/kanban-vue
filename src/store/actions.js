2ecbd634752e89fd7890b872e92890d217508ab1
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2021-01-24 00:45:09 +0900

    add testCode

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,32 +4,32 @@ import { Auth, List, Task } from '../api'
 /* eslint-disable no-unused-vars */
 
 export default {
-  login: {{ commit }} => {
+  login: ({ commit })  => {
     // TODO:
     throw new Error('login action should be implemented')
   },
 
-  fetchLists: {{ commit }} => {
+  fetchLists: ({ commit })  => {
     // TODO:
     throw new Error('fetchLists action should be implemented')
   },
 
-  addTask: {{ commit }} => {
+  addTask: ({ commit })  => {
     // TODO:
     throw new Error('addTask action should be implemented')
   },
 
-  updateTask: {{ commit }} => {
+  updateTask: ({ commit })  => {
     // TODO:
     throw new Error('updateTask action should be implemented')
   },
 
-  removeTask: {{ commit }} => {
+  removeTask: ({ commit })  => {
     // TODO:
     throw new Error('removeTask action should be implemented')
   },
 
-  logout: {{ commit }} => {
+  logout: ({ commit })  => {
     // TODO:
     throw new Error('logout action should be implemented')
   },
